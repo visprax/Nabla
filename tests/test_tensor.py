773806824edbc802ad791d6ca9d830b28faa21d4
saliei <saeid.aliei@gmail.com>
2023-05-15 15:35:00 +0200

    fix subtest

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -6,9 +6,12 @@ from nabla.tensor import Tensor
 class TestNabla(unittest.TestCase):
     def test_tensor_init(self):
         a = Tensor([1.0, 2.0], dtype=np.float64, requires_grad=False)
-        assert np.isclose(a.data[0], 1.0)
-        assert a.requires_grad == False
-        assert a.dtype == np.float64
+        with self.subTest():
+            assert np.isclose(a.data[0], 1.0)
+        with self.subTest():
+            assert a.requires_grad == False
+        with self.subTest():
+            assert a.dtype == np.float64
 
 if __name__ == "__main__":
     unittest.main()
