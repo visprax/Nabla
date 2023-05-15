bb0aab385f8a9f0d01fa811e709935349c9f8298
saliei <saeid.aliei@gmail.com>
2023-05-15 16:15:22 +0200

    add dtype and shape properties

--- nabla/tensor.py
+++ nabla/tensor.py
@@ -3,10 +3,17 @@ import numpy as np
 
 class Tensor:
     def __init__(self, data, dtype=None, requires_grad=None):
-        self.data = data
-        self.dtype = dtype
+        self.data = np.array(data, dtype=dtype if dtype else np.float32)
         self.requires_grad = requires_grad
         self.grad = None
 
     def __repr__(self):
         return f"<Tensor data={self.data} grad={self.grad} dtype={self.dtype}>"
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def dtype(self):
+        return self.data.dtype
