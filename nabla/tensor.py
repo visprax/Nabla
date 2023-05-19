c480ac109a497ed11c957d8949d7b8dd63a1bfa1
saliei <saeid.aliei@gmail.com>
2023-05-19 15:05:42 +0200

    start from a simple tensor class

--- nabla/tensor.py
+++ nabla/tensor.py
@@ -1,29 +1,23 @@
 import numpy as np
-from nabla.function import Add
-from nabla.ops import Ops
 
 class Tensor:
-    def __init__(self, data, dtype=None, requires_grad=None):
-        self.data = np.array(data, dtype=dtype if dtype else np.float32)
+    _default_type = np.float32
+    def __init__(self, data, dtype=None, _parents=(), _label=None, _op=None, requires_grad=None):
+        self.data = data
+        self.dtype = dtype if dtype else Tensor._default_type
+        self._parents = set(_parents)
+        self._label = _label
+        self._op = _op
         self.requires_grad = requires_grad
-        self.grad = None
+        self.grad = 0.0
 
     def __repr__(self):
-        return f"<Tensor data={self.data} grad={self.grad} dtype={self.dtype}>"
-
-    @property
-    def shape(self):
-        return self.data.shape
-
-    @property
-    def dtype(self):
-        return self.data.dtype
+        return f"<Tensor data={self.data} label={self._label} grad={self.grad} parents={self._parents} op={self._op} requires_grad={self.requires_grad}>"
 
     def __add__(self, other):
-        return self._apply(Ops.ADD, other)
+        result = self.data + other.data
+        output = Tensor(result, dtype=result.dtype, _parents=(self, other), _op="ADD")
+        return output
 
-    def _apply(self, op, other):
-        if op == Ops.ADD:
-            out = Add.forward(self, other)
-        return out
-
+    def __radd__(self, other):
+        return self + other
