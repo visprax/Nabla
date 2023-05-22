63669aed2d44bb2802ccbd9b56c6cb376b92afea
saliei <saeid.aliei@gmail.com>
2023-05-22 21:05:47 +0200

    add function module

---
+++ nabla/function.py
@@ -0,0 +1,17 @@
+from nabla import tensor
+
+class Function:
+    def __init__(self, *tensors):
+        self.parents = tensors
+
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError(f"forward method not implemented")
+
+    def backward(self, *args, **kwargs):
+        raise NotImplementedError(f"backward method not implemented")
+
+    @classmethod
+    def apply(func, *tensors, **kwargs):
+        op = func(*tensors)
+        result = tensor.Tensor(op.forward(*[t.data for t in tensors]), _parents=op.parents, **kwargs)
+        return result
